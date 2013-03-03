1dfc32fddd815c3e1dfade2296e9174fa54dc47d
BlueMonday <stevenerenst@gmail.com>
2013-03-02 19:42:39 -0800

    added file opening

--- src/cpu-sch-sim.cpp
+++ src/cpu-sch-sim.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 
-int main void(){
+int main(){
 	using namespace std;
+	string filename;
+	ifstream infile;
+
+	cout<<"Please enter workload filename"<<endl;
+	cin>>filename;
+
+	infile.open(filename.c_str());
+	if(infile.is_open())
+		cout<<"open success"<<endl;
+	else
+		cout<<"File not found"<<endl;
+
+	infile.close();
+	return 0;
 
 }
