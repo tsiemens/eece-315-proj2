e2fbb51e4d1dd9211ba205d0faf351aea5db9c6a
BlueMonday <stevenerenst@gmail.com>
2013-03-02 20:37:26 -0800

    added algorithm selection

--- src/cpu-sch-sim.cpp
+++ src/cpu-sch-sim.cpp
@@ -4,11 +4,16 @@
 
 int main(){
 	using namespace std;
+	int algorithm_index;
 	string filename;
 	ifstream infile;
 
-	cout<<"Please enter workload filename"<<endl;
+	cout<<"Please enter workload filename:";
 	cin>>filename;
+
+	cout<<endl<<"\t"<<"1) FCFS 2) RR 3) Polite Premptive Priority 4) Impatient Premptive Priority 5) Non Premprive Priority 6) SJF 7) SPB"<<endl<<endl;	
+	cout<<"Select scheduling algorithm:";
+	cin>>algorithm_index;	
 
 	infile.open(filename.c_str());
 	if(infile.is_open())
