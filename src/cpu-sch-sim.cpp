48a26c89f6bfc37c464215dd0a0d02845d7f2f08
Steven Erenst <stevenerenst@gmail.com>
2013-03-07 02:43:09 -0800

    added parsing of workload in main

--- src/cpu-sch-sim.cpp
+++ src/cpu-sch-sim.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<vector>
+#include "PCB.h"
+#include "Workload_Parser.h"
 
 int main(){
 	using namespace std;
 	int algorithm_index;
+	Workload_Parser parser;
+	vector<PCB> processes;
 	string filename;
 	ifstream infile;
+	
+	cout<<"Please enter workload filename: ";
+	do{
+		cin>>filename;
+	}while(!(Workload_Parser::valid_file_name(filename)));
 
-	cout<<"Please enter workload filename:";
-	cin>>filename;
-
+	processes = parser.parse_workload(filename);
+	
 	cout<<endl<<"\t"<<"1) FCFS 2) RR 3) Polite Premptive Priority 4) Impatient Premptive Priority 5) Non Premprive Priority 6) SJF 7) SPB"<<endl<<endl;	
 	cout<<"Select scheduling algorithm:";
-	cin>>algorithm_index;	
+	cin>>algorithm_index;
+	
 
-	infile.open(filename.c_str());
-	if(infile.is_open())
-		cout<<"open success"<<endl;
-	else
-		cout<<"File not found"<<endl;
-
-	infile.close();
 	return 0;
-
 }
