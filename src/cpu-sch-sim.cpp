45de55f9e791e91ebfbdff309e19e758805c05a3
Steven Erenst <stevenerenst@gmail.com>
2013-03-07 03:13:50 -0800

    workload parser now  returns vector of pcb pointer and fixed memory leak

--- src/cpu-sch-sim.cpp
+++ src/cpu-sch-sim.cpp
@@ -9,7 +9,7 @@ int main(){
 	using namespace std;
 	int algorithm_index;
 	Workload_Parser parser;
-	vector<PCB> processes;
+	vector<PCB*> processes;
 	string filename;
 	ifstream infile;
 	
@@ -19,11 +19,13 @@ int main(){
 	}while(!(Workload_Parser::valid_file_name(filename)));
 
 	processes = parser.parse_workload(filename);
-	
+
 	cout<<endl<<"\t"<<"1) FCFS 2) RR 3) Polite Premptive Priority 4) Impatient Premptive Priority 5) Non Premprive Priority 6) SJF 7) SPB"<<endl<<endl;	
 	cout<<"Select scheduling algorithm:";
 	cin>>algorithm_index;
 	
+	for(unsigned int i=0; i< processes.size();i++)
+		delete processes[i];
 
 	return 0;
 }
